60557edab63a8167a9d5dee91ae32a6cdc592e9a
James Hunter <jamessahunter@gmail.com>
2023-11-03 17:09:54 -0700

    make homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,26 +1,19 @@
 const router = require('express').Router();
-const { Gallery, Painting } = require('../models');
+const { User, Post } = require('../models');
 // Import the custom middleware
 const {withAuth, areAuth } = require('../utils/auth');
 // router.use(withAuth);
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
   try {
-    const dbGalleryData = await Gallery.findAll({
-      include: [
-        {
-          model: Painting,
-          attributes: ['filename', 'description'],
-        },
-      ],
-    });
+    const dbPostData = await Post.findAll();
 
-    const galleries = dbGalleryData.map((gallery) =>
-      gallery.get({ plain: true })
+    const posts = dbPostData.map((post) =>
+      post.get({ plain: true })
     );
 
     res.render('homepage', {
-      galleries,
+      posts,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
